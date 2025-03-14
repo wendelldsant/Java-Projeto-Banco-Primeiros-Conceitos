7e82ebd5407b474492d80499f7e88410a7fe0643
Wendell dos Santos <wendell_dsantos@hotmail.com>
2025-03-14 15:04:04 -0300

    Adiciona visualizacao de dados cadastrais

--- untitled/src/cp1/encapsulamento/Main.java
+++ untitled/src/cp1/encapsulamento/Main.java
@@ -21,8 +21,8 @@ public class Main {
         System.out.println("********************************");
 
         int userChoice = 0;
-        while (userChoice != 5){
-            String[] menu = {"Consultar saldo", "Depósito", "Saque", "Alterar Titular", "Sair"};
+        while (userChoice != 6){
+            String[] menu = {"Consultar saldo", "Depósito", "Saque", "Alterar Titular", "Dados Cadastrais", "Sair"};
             System.out.println("\nEscolha uma das opções abaixo");
             for (int i = 0; i < menu.length; i++) {
                 System.out.println((i+1) + ". " + menu[i]);
@@ -33,30 +33,34 @@ public class Main {
             switch (userChoice){
                 case 1:
                     double saldo = conta.consultarSaldo();
-                    System.out.println("Seu saldo é de R$ " + saldo);
+                    System.out.println("\nSeu saldo é de R$ " + saldo);
                     break;
                 case 2:
-                    System.out.print("Digite o valor de depósito: ");
+                    System.out.print("\nDigite o valor de depósito: ");
                     double valor = leitura.nextDouble();
                     conta.depositar(valor);
                     break;
                 case 3:
-                    System.out.print("Digite o valor que você deseja sacar: ");
+                    System.out.print("\nDigite o valor que você deseja sacar: ");
                     valor = leitura.nextDouble();
                     conta.sacar(valor);
                     break;
                 case 4:
-                    System.out.print("Digite o novo nome de titular: ");
+                    System.out.print("\nDigite o novo nome de titular: ");
                     leitura.nextLine();
                     titular = leitura.nextLine();
                     conta.setTitular(titular);
                     System.out.println("Titular alterado com sucesso. O novo titular é " + conta.getTitular());
                     break;
                 case 5:
+                    System.out.println("\nTitular da Conta: " + conta.getTitular());
+                    System.out.println("Saldo: R$ " + conta.consultarSaldo());
+                    break;
+                case 6:
                     System.out.println("Encerrando login...");
                     break;
                 default:
-                    System.out.println("Opção inválida.");
+                    System.out.println("\nOpção inválida!");
                     break;
             }
 
